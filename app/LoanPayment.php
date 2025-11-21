d49974ccfa970e4ed8f9c4ea2f543a2d148b56f0
Richard Velasquez <revelasquez666@gmail.com>
2025-11-21 12:50:01 -0400

    calculos de interes penal v2

--- app/LoanPayment.php
+++ app/LoanPayment.php
@@ -285,6 +285,7 @@ class LoanPayment extends Model
     public function categorie()
     {
         return $this->belongsTo(LoanPaymentCategorie::class, 'categorie_id');
+    }
     public static function penal_by_paid($penal_interest, $days, $balance, $denominator)
     {
         return Util::round2(((($penal_interest/100)/$denominator)*$days*$balance),8);
